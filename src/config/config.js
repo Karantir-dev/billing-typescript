abdd179b1fbfc5eaba7fb1d318b1eca08182732e
Sergey <sera.ua09@gmail.com>
2022-04-26 10:25:40 +0300

    Changed ci/cd

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,4 @@
-export const BASE_URL = 'https://test.hardsoft.cf'
-export const RECAPTCHA_KEY = '6LdIo4QeAAAAAGaR3p4-0xh6dEI75Y4cISXx3FGR'
-//export const RECAPTCHA_KEY = '6LduamkfAAAAAKOy-2RHeTh6GLCydKT8fhw--KuW'
+// export const BASE_URL = 'https://test.hardsoft.cf'
+// export const RECAPTCHA_KEY = '6LdIo4QeAAAAAGaR3p4-0xh6dEI75Y4cISXx3FGR'
+export const BASE_URL = 'https://cp.zomro.com'
+export const RECAPTCHA_KEY = '6LduamkfAAAAAKOy-2RHeTh6GLCydKT8fhw--KuW'
