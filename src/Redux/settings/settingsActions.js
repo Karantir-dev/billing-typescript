aa0f2f82c811a2a81098c850bf540fb0c85a06fa
Mykhailo Tatochenko <mihail@advicera.org>
2022-05-02 09:32:10 +0300

    fixed time

--- src/Redux/settings/settingsActions.js
+++ src/Redux/settings/settingsActions.js
@@ -4,7 +4,7 @@ const setUsersEdit = createAction('SET_USER_EDIT')
 const setUsersParams = createAction('SET_USER_PARAMS')
 
 const setUpdateTime = createAction('SET_UPDATE_TIME')
-const setUpdateAvatar = createAction('SET_UPDATE_TIME')
+const setUpdateAvatar = createAction('SET_UPDATE_AVATAR')
 
 export default {
   setUsersEdit,
