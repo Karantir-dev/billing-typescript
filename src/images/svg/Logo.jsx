d48561a87d0712a5426389b14e97cbd06d361b1e
Serhii <95615274+olegzomrodev@users.noreply.github.com>
2022-05-06 15:18:07 +0300

    canceled changes

--- src/images/svg/Logo.jsx
+++ src/images/svg/Logo.jsx
@@ -224,7 +224,7 @@ export default function SvgComponent(props) {
     <svg
       width={105}
       height={48}
-      viewBox="0 0 110 48"
+      viewBox="0 0 100 100"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
