d4a2dcc1793cdc74500d6c72ac94d49bffebe67c
seraus09 <sera.ua09@gmail.com>
2022-05-26 11:39:38 +0300

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,8 @@
 // ------- dev -----
-export const BASE_URL = 'https://test.hard-soft.cf'
-export const RECAPTCHA_KEY = '6LdIo4QeAAAAAGaR3p4-0xh6dEI75Y4cISXx3FGR'
+// export const BASE_URL = 'https://test.hard-soft.cf'
+// export const RECAPTCHA_KEY = '6LdIo4QeAAAAAGaR3p4-0xh6dEI75Y4cISXx3FGR'
 
 // ---- production ------
 //export const BASE_URL = 'https://cp.zomro.com'
-// export const BASE_URL = 'https://billmgr.had.su'
-// export const RECAPTCHA_KEY = '6LduamkfAAAAAKOy-2RHeTh6GLCydKT8fhw--KuW'
+export const BASE_URL = 'https://billmgr.had.su'
+export const RECAPTCHA_KEY = '6LduamkfAAAAAKOy-2RHeTh6GLCydKT8fhw--KuW'
