58cae84167cc8b466d5c0828ba7da1b0d9fa173d
System Administrator <root@Sergijs-MacBook-Air.local>
2022-07-06 11:23:22 +0300

    update

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 // ------- dev -----
-// export const BASE_URL = 'https://test.hard-soft.cf'
-// export const RECAPTCHA_KEY = '6LdIo4QeAAAAAGaR3p4-0xh6dEI75Y4cISXx3FGR'
+export const BASE_URL = 'https://test.hard-soft.cf'
+export const RECAPTCHA_KEY = '6LdIo4QeAAAAAGaR3p4-0xh6dEI75Y4cISXx3FGR'
 
 // ---- production ------
-export const BASE_URL = 'https://cp.zomro.com'
-export const RECAPTCHA_KEY = '6LduamkfAAAAAKOy-2RHeTh6GLCydKT8fhw--KuW'
+//export const BASE_URL = 'https://cp.zomro.com'
+//export const RECAPTCHA_KEY = '6LduamkfAAAAAKOy-2RHeTh6GLCydKT8fhw--KuW'
