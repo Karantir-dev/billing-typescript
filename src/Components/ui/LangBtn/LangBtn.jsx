d851d663954316d980bc2741006b2be10e878b27
Serhii <95615274+olegzomrodev@users.noreply.github.com>
2022-07-07 13:15:39 +0300

    fixes, all services have the same view, the same order, langs translate added, fixes from file

--- src/Components/ui/LangBtn/LangBtn.jsx
+++ src/Components/ui/LangBtn/LangBtn.jsx
@@ -11,7 +11,6 @@ const LANGUAGES = ['en', 'ru', 'kz', 'ua']
 export default function LangBtn({ burgerType, authType, mainType }) {
   const { i18n } = useTranslation()
   const currentLang = i18n.language?.slice(0, 2)
-  console.log(i18n)
 
   if (!LANGUAGES.includes(currentLang)) {
     i18n.changeLanguage('en')
