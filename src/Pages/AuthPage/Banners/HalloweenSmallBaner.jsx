a5585df5d7f01bbd699f1b1d9d3ba68677c73869
Mykhailo Tatochenko <mihail@advicera.org>
2022-10-28 14:34:24 +0300

    halloween baner

---
+++ src/Pages/AuthPage/Banners/HalloweenSmallBaner.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Pumpkins from './Pumpkins'
+import PumpkinsText from './PumpkinsText'
+import { useTranslation } from 'react-i18next'
+
+import s from '../AuthPage.module.scss'
+
+export default function Component() {
+  const { t } = useTranslation('other')
+
+  return (
+    <>
+      <Pumpkins />
+      <PumpkinsText className={s.pumpkinsTextIcon} />
+      <div className={s.pumpkinsText}>
+        <div>{t('discounts_up_to', { amount: '-70%' })}</div>
+        <div>{t('halloween_discount')}</div>
+      </div>
+    </>
+  )
+}
