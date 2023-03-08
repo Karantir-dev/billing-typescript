99c8f68296920fb843db472b524414e750655ed4
Ihor Bozhuk <ihor.bozhuk@zomro.org>
2023-03-08 14:11:21 +0200

    dedic modal button was disabled when product inactive

--- src/Components/Services/DedicatedServers/ProlongModal/ProlongModal.jsx
+++ src/Components/Services/DedicatedServers/ProlongModal/ProlongModal.jsx
@@ -242,6 +242,7 @@ export default function ProlongModal({
               </div>
               <div className={s.btns_wrapper}>
                 <Button
+                  disabled={initialState?.status?.$ !== 'status_2'}
                   className={s.buy_btn}
                   isShadow
                   size="medium"
