a01201997552bb4be9a273f37d9dee6db17354b7
Oleksii Rozhok <oleksii.rozhok@gmail.com>
2023-04-26 10:14:58 +0300

    fix prolong period in multiselect/Oleksii

--- src/Redux/cart/cartOperations.js
+++ src/Redux/cart/cartOperations.js
@@ -1,6 +1,12 @@
 import qs from 'qs'
 import i18n from './../../i18n'
-import { actions, cartActions, billingOperations, userOperations, billingActions } from '..'
+import {
+  actions,
+  cartActions,
+  billingOperations,
+  userOperations,
+  billingActions,
+} from '..'
 import axios from 'axios'
 import { axiosInstance } from '../../config/axiosInstance'
 import { toast } from 'react-toastify'
