f2e2256d462354617ce884b3cb6e529fc98196f4
Mykhailo Tatochenko <mihail@advicera.org>
2023-05-09 16:40:52 +0300

    comment analytics screen

--- src/Components/Billing/SuccessPayment/SuccessPayment.jsx
+++ src/Components/Billing/SuccessPayment/SuccessPayment.jsx
@@ -15,9 +15,9 @@ export default function Component() {
   const dispatch = useDispatch()
   const navigate = useNavigate()
 
-  const refillId = cookies.getCookie('payment_id')
+  const refillId = cookies.getCookie('payment_id') // payment ID, obtained during the transition to the payment system
 
-  const paymentsList = useSelector(billingSelectors.getPaymentsList)
+  const paymentsList = useSelector(billingSelectors.getPaymentsList) // list of all user payments
 
   const [paymentId, setPaymentId] = useState(null)
   const [cartData, setCartData] = useState(null)
@@ -32,36 +32,44 @@ export default function Component() {
   }, [])
 
   useEffect(() => {
+    // in the list of payments we look for the necessary payment
     if (paymentsList && paymentsList?.length > 0) {
       const item = paymentsList?.find(e => e?.id?.$ === refillId)
 
       if (item) {
         if (item?.billorder) {
+          //if the billorder field is present in the payment, then this is a purchase through the basket
           const data = cookies.getCookie(`cartData_${refillId}`)
+          // check whether we have such a basket saved (baskets are stored for 5 days from the moment of its creation)
           if (data) {
+            //if we find such a basket, then we save
             const dataJson = JSON.parse(data)
             setCartData(dataJson)
           }
         }
-        setPaymentId(item)
+        setPaymentId(item) // write down the rest of the payment data
       }
     }
   }, [paymentsList])
 
   useEffect(() => {
     if (paymentId) {
-      const currency = paymentId?.paymethodamount_iso?.$?.includes('RUB') ? 'RUB' : 'EUR'
-      const value = paymentId?.paymethodamount_iso?.$?.replace(currency, '')
-      const tax = paymentId?.tax?.$?.replace(currency, '')
-      window.dataLayer.push({ ecommerce: null })
+      //if we have payment
+      const currency = paymentId?.paymethodamount_iso?.$?.includes('RUB') ? 'RUB' : 'EUR' //check what currency used
+      const value = paymentId?.paymethodamount_iso?.$?.replace(currency, '') //get the payment amount
+      const tax = paymentId?.tax?.$?.replace(currency, '') //get the payment tax
+      window.dataLayer.push({ ecommerce: null }) //clean data layer ecommerce
 
       if (paymentId?.billorder) {
+        // we check that the payment comes from the cart
         if (cartData?.billorder === paymentId?.billorder?.$) {
+          // we check if we have such a basket and send it to analytics along with the items
           window.dataLayer.push({
             event: 'purchase',
             ecommerce: {
               transaction_id:
                 refillId || `No payment id (billorder: ${cartData?.billorder})`,
+              //if suddenly there is no payment ID, we will send the basket ID (probability is very small)
               affiliation: 'cp.zomro.com',
               value: Number(value) || 0,
               tax: Number(tax) || 0,
@@ -72,8 +80,9 @@ export default function Component() {
             },
           })
 
-          cookies.eraseCookie(`cartData_${refillId}`)
+          cookies.eraseCookie(`cartData_${refillId}`) // if the cart was used, then clear it
         } else {
+          // if there is no such basket, we record the payment data but without the items that were in the basket, and without promocode
           window.dataLayer.push({
             event: 'purchase',
             ecommerce: {
@@ -92,12 +101,13 @@ export default function Component() {
                   item_category: 'Lost data' || '',
                   quantity: 1,
                 },
-              ],
+              ], // items if we did not find the basket
             },
           })
         }
       } else {
         if (refillId && !cartData) {
+          // if this payment is not from the cart we send the payment ID and record the refill of the account in the items
           window.dataLayer.push({
             event: 'purchase',
             ecommerce: {
@@ -119,7 +129,7 @@ export default function Component() {
           })
         }
       }
-      cookies.eraseCookie('payment_id')
+      cookies.eraseCookie('payment_id') // if the payment id was used, then clear it
     }
   }, [paymentId])
 
