0d8f6ec2480fcec09728ba2b230b682cf126c231
vitaliydev-zomro <vitaliy.dev@server-panel.net>
2023-09-11 13:04:09 +0300

    adds purchase fb event

--- src/utils/hooks/useAnalyticsSender.js
+++ src/utils/hooks/useAnalyticsSender.js
@@ -47,8 +47,8 @@ export default function useAnalyticsSender() {
   useEffect(() => {
     if (paymentItem) {
       //if we have payment
-      let value = paymentItem?.subaccountamount_iso?.$.replace('EUR', '')
-      let tax = paymentItem?.tax?.$.replace('EUR', '')
+      let value = Number(paymentItem?.subaccountamount_iso?.$.replace('EUR', ''))
+      let tax = Number(paymentItem?.tax?.$.replace('EUR', ''))
 
       window?.dataLayer?.push({ ecommerce: null }) //clean data layer ecommerce
 
@@ -58,8 +58,8 @@ export default function useAnalyticsSender() {
           payment_type: paymentItem?.paymethod_name?.$,
           transaction_id: paymentId,
           affiliation: window.location.hostname,
-          value: Number(value) || 0,
-          tax: Number(tax) || 0,
+          value: value || 0,
+          tax: tax || 0,
           currency: 'EUR',
           shipping: '0',
           coupon: '',
@@ -75,6 +75,13 @@ export default function useAnalyticsSender() {
         },
       }
 
+      const fbAnalytics = {
+        value: value,
+        currency: 'EUR',
+        content_type: 'Service',
+        content_category: 'lost_data',
+      }
+
       // If it is a purchase of services
       if (paymentItem?.billorder) {
         // If there is saved product data
@@ -85,12 +92,31 @@ export default function useAnalyticsSender() {
           window?.dataLayer?.push(analyticsData)
           axios.post(`${API_URL}/api/analytic/add/`, analyticsData)
 
+          if (window.fbq) {
+            fbAnalytics.contents = cartData?.items?.map(el => ({
+              id: el.item_id,
+              quantity: 1,
+              name: el.item_name,
+              price: el.price,
+            }))
+            fbAnalytics.content_category = cartData?.items?.[0]?.item_category
+
+            window.fbq('track', 'Purchase', fbAnalytics)
+          } else {
+            console.log('fbq absent 1 ')
+          }
+
           cookies.eraseCookie(`cartData_${paymentId}`)
 
           // If there is NO saved product data
         } else {
           window?.dataLayer?.push(analyticsData)
           axios.post(`${API_URL}/api/analytic/add/`, analyticsData)
+          if (window.fbq) {
+            window.fbq('track', 'Purchase', fbAnalytics)
+          } else {
+            console.log('fbq absent 2 ')
+          }
         }
 
         // If it is a balance replenishment (we don`t have saved product data)
@@ -100,14 +126,22 @@ export default function useAnalyticsSender() {
             {
               item_name: 'Refill',
               item_id: paymentId,
-              price: Number(value) || 0,
+              price: value || 0,
               item_category: 'Refill',
               quantity: 1,
             },
           ]
+          fbAnalytics.content_category = 'Refill'
+          fbAnalytics.content_type = 'Refill'
+          fbAnalytics.content_ids = [paymentId]
 
           window?.dataLayer?.push(analyticsData)
           axios.post(`${API_URL}/api/analytic/add/`, analyticsData)
+          if (window.fbq) {
+            window.fbq('track', 'Purchase', fbAnalytics)
+          } else {
+            console.log('fbq absent 3 ')
+          }
         }
       }
       cookies.eraseCookie('payment_id') // if the payment id was used, then clear it
