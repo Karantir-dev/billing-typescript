5db517795e963d7e8724f81818c41305c097aed3
Oleksii-Rozhok94 <130983324+Oleksii-Rozhok94@users.noreply.github.com>
2023-10-04 13:39:22 +0300

    Update src/Redux/dedicatedServers/dedicOperations.js

--- src/Redux/dedicatedServers/dedicOperations.js
+++ src/Redux/dedicatedServers/dedicOperations.js
@@ -89,7 +89,7 @@ const getTarifs = (setNewVds, signal, setIsLoading) => (dispatch, getState) => {
       const { elem: tarifList } =
         data.doc.list.find(el => el?.$name === 'tariflist') || {}
       const { val: datacenter } =
-        data.doc.slist.find(el => el?.$name === 'datacenter') || []
+        data.doc.slist.find(el => el?.$name === 'datacenter') || {}
       const { val: period } = data.doc.slist.find(el => el.$name === 'period') || []
 
       const { $: currentDatacenter } = data.doc.datacenter
