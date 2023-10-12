9a5b99a6c16ddf0a4e2a2eafb3fb241a7aae75e6
vitaliydev-zomro <vitaliy.dev@server-panel.net>
2023-10-12 10:37:21 +0300

    adds blocking modal styles

--- src/Components/BlockingModal/BlockingModal.jsx
+++ src/Components/BlockingModal/BlockingModal.jsx
@@ -1,14 +1,19 @@
-import { Modal } from '@components'
+import { Icon, Modal } from '@components'
 import { selectors } from '@redux'
 import { t } from 'i18next'
 import { useSelector } from 'react-redux'
+import s from './BlockingModal.module.scss'
 
 export default function BlockingModal() {
   const isOnline = useSelector(selectors.onlineStatus)
 
   return (
-    <Modal isOpen={!isOnline}>
-      <Modal.Body>{t('offline', { ns: 'other' })}</Modal.Body>
+    <Modal className={s.modal} isOpen={!isOnline}>
+      <Modal.Body className={s.modal_body}>
+        <Icon className={s.triangle} name="ErrorPay" />
+        <p className={s.title}>{t('offline_title', { ns: 'other' })}</p>
+        <p className={s.text}>{t('offline', { ns: 'other' })}</p>
+      </Modal.Body>
     </Modal>
   )
 }
--- src/images/svg/ErrorPay.jsx
+++ src/images/svg/ErrorPay.jsx
@@ -3,15 +3,44 @@ export default function SvgComponent(props) {
     <svg
       width={117}
       height={117}
+      viewBox="0 0 117 117"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
       <path
-        d="M59.5 63.3h-2.05l-.65-24.65h3.35L59.5 63.3Zm.5 8.5c-.433.433-.95.65-1.55.65-.6 0-1.117-.217-1.55-.65-.4-.433-.6-.95-.6-1.55 0-.567.2-1.067.6-1.5.433-.433.95-.65 1.55-.65.6 0 1.117.217 1.55.65.467.433.7.933.7 1.5 0 .6-.233 1.117-.7 1.55Z"
-        fill="#D93F21"
+        d="M59.5 63.3H57.45L56.8 38.65H60.15L59.5 63.3ZM60 71.8C59.5667 72.2333 59.05 72.45 58.45 72.45C57.85 72.45 57.3333 72.2333 56.9 71.8C56.5 71.3667 56.3 70.85 56.3 70.25C56.3 69.6833 56.5 69.1833 56.9 68.75C57.3333 68.3167 57.85 68.1 58.45 68.1C59.05 68.1 59.5667 68.3167 60 68.75C60.4667 69.1833 60.7 69.6833 60.7 70.25C60.7 70.85 60.4667 71.3667 60 71.8Z"
+        fill="url(#paint0_linear_11278_28940)"
       />
-      <path d="M9.57 86.75 58.5 2l48.93 84.75H9.57Z" stroke="#D93F21" strokeWidth={2} />
+      <path
+        d="M9.56956 86.75L58.5 2L107.43 86.75H9.56956Z"
+        stroke="url(#paint1_linear_11278_28940)"
+        strokeWidth="2"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_11278_28940"
+          x1="57.9082"
+          y1="29.7931"
+          x2="57.6091"
+          y2="71.9979"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#FF6F66" />
+          <stop offset="0.982292" stopColor="#FA487D" />
+        </linearGradient>
+        <linearGradient
+          id="paint1_linear_11278_28940"
+          x1="63.807"
+          y1="14.1207"
+          x2="63.6702"
+          y2="117"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#FF6F66" />
+          <stop offset="0.982292" stopColor="#FA487D" />
+        </linearGradient>
+      </defs>
     </svg>
   )
 }
