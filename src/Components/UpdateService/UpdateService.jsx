d2dff93c12474c6388756ee2d3a4551d08bf5851
Igor Bondarenko <igorbondarenko35@gmail.com>
2023-11-13 15:12:58 +0200

    changed info plug and added info about vds xxl

--- src/Components/UpdateService/UpdateService.jsx
+++ src/Components/UpdateService/UpdateService.jsx
@@ -4,16 +4,12 @@ import { Icon } from '@components'
 
 import s from './UpdateService.module.scss'
 
-import {
-  userSelectors,
-  supportSelectors,
-  supportOperations,
-} from '@redux'
+import { userSelectors, supportSelectors, supportOperations } from '@redux'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import * as route from '@src/routes'
 import { useCancelRequest } from '@src/utils'
-import { USERS_WITH_G7 } from '@utils/constants'
+import { USERS_WITH_G7, USERS_WITH_G8 } from '@utils/constants'
 
 export default function UpdateService() {
   const { t } = useTranslation('other')
@@ -29,30 +25,69 @@ export default function UpdateService() {
   const { $id: ownerId, $realname: userName } = useSelector(userSelectors.getUserInfo)
   const tickerList = useSelector(supportSelectors.getTicketList)
 
-  const g7Ticket = tickerList?.find(obj => {
+  const isG7TicketOpened = tickerList?.find(obj => {
     const name = obj?.name?.$
-    return name?.includes('Config 47') && name?.includes('Config 43') && name?.includes('VDS XL')
+    return (
+      name?.includes('Config 47') &&
+      name?.includes('Config 43') &&
+      name?.includes('VDS XL')
+    )
   })
 
-  const ticketId = g7Ticket?.id?.$
+  const isG8TicketOpened = tickerList?.find(obj => {
+    const name = obj?.name?.$
+    return (
+      name?.includes('Config 48') &&
+      name?.includes('Config 50') &&
+      name?.includes('VDS XXL')
+    )
+  })
 
-  const isUserIdInList = USERS_WITH_G7?.includes(Number(ownerId))
+  const g7TicketId = isG7TicketOpened?.id?.$
+  const g8TicketId = isG8TicketOpened?.id?.$
 
-  return isUserIdInList && g7Ticket ? (
-    <button
-      className={s.wrapper}
-      type="button"
-      onClick={() =>
-        navigate(`${route.SUPPORT}/requests/${ticketId}`)
-      }
-    >
-      <Icon name="Attention" className={s.icon} />
-      <p className={s.text}>
-        {t('Dear')} {userName}{'.'} {t('Update your server')}{' '}
-        <span to={`${route.SUPPORT}/requests/${g7Ticket?.id?.$}`} className={s.link}>
-          {t('here')}
-        </span>
-      </p>
-    </button>
-  ) : null
+  const isUserIdInListWithG7 = USERS_WITH_G7?.includes(Number(ownerId))
+  const isUserIdInListWithG8 = USERS_WITH_G8?.includes(Number(ownerId))
+
+  const g7Ticket = isUserIdInListWithG7 && isG7TicketOpened
+  const g8Ticket = isUserIdInListWithG8 && isG8TicketOpened
+
+  return (
+    <>
+      {g7Ticket || g8Ticket ? (
+        <button
+          className={s.wrapper}
+          type="button"
+          onClick={() => {
+            if (g7Ticket && g8Ticket) {
+              return
+            }
+
+            if (g7Ticket) {
+              return navigate(`${route.SUPPORT}/requests/${g7TicketId}`)
+            }
+
+            return navigate(`${route.SUPPORT}/requests/${g8TicketId}`)
+          }}
+        >
+          <Icon name="Attention" className={s.icon} />
+          <p className={s.text}>
+            {t('Dear')} {userName}
+            {'.'} {t('Update your server')}{' '}
+            {g7Ticket && (
+              <a href={`${route.SUPPORT}/requests/${g7TicketId}`} className={s.link}>
+                {'Config 47'}
+              </a>
+            )}
+            {g7Ticket && g8Ticket && ' & '}
+            {g8Ticket && (
+              <a href={`${route.SUPPORT}/requests/${g8TicketId}`} className={s.link}>
+                {'Config 48'}
+              </a>
+            )}
+          </p>
+        </button>
+      ) : null}
+    </>
+  )
 }
