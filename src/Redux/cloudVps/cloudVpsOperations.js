9b0c34535e87c340ed5892f1c1b59c58ea599091
Vitalii😎 <vitaliy.almiz@zomro.org>
2024-04-22 15:25:59 +0300

    adds licence_agreement: 'on' for clouds

--- src/Redux/cloudVps/cloudVpsOperations.js
+++ src/Redux/cloudVps/cloudVpsOperations.js
@@ -659,7 +659,7 @@ const setOrderData =
           sok: 'ok',
           lang: 'en',
           order_period: '-50',
-
+          licence_agreement: 'on',
           ...orderData,
         }),
         { signal },
