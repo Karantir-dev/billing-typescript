87fd7cb1909fe3beead9347b3a464af1c805baaa
Igor Bondarenko <igorbondarenko35@gmail.com>
2024-04-29 13:27:14 +0300

    site care is back on the menu. Sales will continue

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -229,7 +229,7 @@ export const TARIFFS_PRICES = {
 }
 
 /* ID of services that should be disabled: */
-export const FORBIDDEN_TO_ORDER_SERVICES = [27090, 6, 27140, 27129]
+export const FORBIDDEN_TO_ORDER_SERVICES = [27090, 6, 27140]
 
 /*
 Below ID and names of all services
