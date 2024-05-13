eebb996cfd98f6b2a0ee17d179e6ae2f72659c7f
Igor Bondarenko <igorbondarenko35@gmail.com>
2024-05-13 11:09:27 +0300

    functions combined

--- src/Redux/userInfo/userOperations.js
+++ src/Redux/userInfo/userOperations.js
@@ -108,13 +108,15 @@ const getActiveServices = (data, dispatch) => {
 
 const funcsArray = [
   userInfo,
-  userNotifications,
+  (...args) => {
+    userNotifications(...args)
+    getActiveServices(...args)
+  },
   currentSessionRights,
   userTickets,
   clearBasket,
   dashBoardInfo,
   getAvailableCredit,
-  getActiveServices,
 ]
 
 const getUserInfo = (sessionId, setLoading, disableClearBasket) => dispatch => {
@@ -137,6 +139,7 @@ const getUserInfo = (sessionId, setLoading, disableClearBasket) => dispatch => {
         out: 'json',
         lang: 'en',
         auth: sessionId,
+        notifiaction: 'count',
       }),
     ),
     axiosInstance.post(
@@ -183,16 +186,6 @@ const getUserInfo = (sessionId, setLoading, disableClearBasket) => dispatch => {
         out: 'json',
         lang: 'en',
         auth: sessionId,
-      }),
-    ),
-    axiosInstance.post(
-      '/',
-      qs.stringify({
-        func: 'notify',
-        out: 'json',
-        lang: 'en',
-        auth: sessionId,
-        notifiaction: 'count',
       }),
     ),
   ])
