7b7076a04e0241d77afc2fb709358af1e6f9853f
Vitalii😎 <vitaliy.almiz@zomro.org>
2024-05-22 19:37:05 +0300

    err handle

--- src/utils/cloud/rewriteCloudsPrices.js
+++ src/utils/cloud/rewriteCloudsPrices.js
@@ -2,21 +2,25 @@ import { TARIFFS_PRICES } from '../constants'
 
 export default function rewriteCloudsPrices(payload) {
   if (payload) {
-    const key = Object.keys(payload)[0]
+    try {
+      const key = Object.keys(payload)[0]
 
-    payload[key] = payload[key]?.map(el => {
-      const cutedName = el.title.main.$.split('|')[0].trim()
+      payload[key] = payload[key]?.map(el => {
+        const cutedName = el.title.main.$.split('|')[0].trim()
 
-      const newDayPrice = TARIFFS_PRICES[cutedName]?.day
-      const newMonthPrice = TARIFFS_PRICES[cutedName]?.month
-      el.title.main.$ = cutedName
-      el.prices.price.cost.$ = String(newDayPrice)
-      el.prices.price.cost.month = String(newMonthPrice)
+        const newDayPrice = TARIFFS_PRICES[cutedName]?.day
+        const newMonthPrice = TARIFFS_PRICES[cutedName]?.month
+        el.title.main.$ = cutedName
+        el.prices.price.cost.$ = String(newDayPrice)
+        el.prices.price.cost.month = String(newMonthPrice)
 
-      return el
-    })
+        return el
+      })
 
-    return payload
+      return payload
+    } catch (err) {
+      console.error(err.message)
+    }
   } else {
     return {}
   }
